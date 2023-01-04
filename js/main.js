1498127a50f3e535a1bf5b33db8ac903220a26e0
sindystar <hasungah@gmail.com>
2023-01-05 00:32:02 +0900

    metro UI 적용

--- js/main.js
+++ js/main.js
@@ -113,3 +113,67 @@ function stopRolling() {
 	btnPlay.classList.remove('on');
 }
 
+// main
+
+const btnMetro = document.querySelectorAll('#metro nav a');
+const boxMetro = document.querySelectorAll('#metro section');
+const btnFooter = document.querySelector('footer .toggle');
+const mapFrame = document.querySelector('#location');
+const mapInfo = mapFrame.querySelector('.mapInfo');
+
+//메트로 탭메뉴 토글 버튼
+btnMetro.forEach((btn, idx) => {
+	btn.addEventListener('click', (e) => {
+		e.preventDefault();
+
+		for (const el of btnMetro) el.classList.remove('on');
+		btnMetro[idx].classList.add('on');
+
+		for (const el of boxMetro) el.classList.remove('on');
+		boxMetro[idx].classList.add('on');
+
+		setTimeout(() => {
+			for (const el of boxMetro) el.classList.remove('active');
+			boxMetro[idx].classList.add('active');
+		}, 0);
+	});
+});
+
+//footer토글버튼 이벤트
+btnFooter.addEventListener('click', (e) => {
+	e.preventDefault();
+	e.currentTarget.parentElement.classList.toggle('on');
+});
+
+//지도 연결 코드
+const mapContainer = document.querySelector('#map');
+const mapOption = {
+	center: new kakao.maps.LatLng(37.5704837, 126.9705674),
+	level: 3,
+};
+const map = new kakao.maps.Map(mapContainer, mapOption);
+
+//마커 인스턴스 생성
+const marker = new kakao.maps.Marker({
+	position: mapOption.center,
+});
+marker.setMap(map);
+
+//지도 줌기능 비활성화 (휠이벤트)
+map.setZoomable(false);
+
+//mapInfo패널 이벤트 연결
+mapInfo.addEventListener('click', () => {
+	mapInfo.classList.add('off');
+	map.setCenter(mapOption.center);
+});
+mapFrame.addEventListener('mouseleave', () => {
+	mapInfo.classList.remove('off');
+});
+
+//브라우저 리사이즈시 지도 항상 가운데 유지
+window.addEventListener('resize', () => {
+	map.setCenter(mapOption.center);
+});
+
+
